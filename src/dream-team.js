9f5387a445ace157a6453299a17b33e357904065
Sergey Kostin <dr.zad1994@gmail.com>
2020-02-25 16:04:35 +0300

    feat: add dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let dreamTeam = '';
+    members.forEach(item => {
+      if (typeof item === 'string') {
+        dreamTeam = `${dreamTeam}${item.trim()[0].toUpperCase()}`;
+      };
+    });
+    return dreamTeam.split('').sort().join('');
+  }
+  return false;
 };
