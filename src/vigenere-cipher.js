f4cdc3527567f58e9e3acf368b807e2f4bb76d02
Sergey Kostin <dr.zad1994@gmail.com>
2020-02-26 11:52:12 +0300

    feat: add vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,69 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+  constructor(direct = true) {
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    this.direct = direct;
+  }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  createLongKey(message, key) {
+    let longKey = key;
+    while (longKey.length < message.length) {
+      longKey = `${longKey}${key}`;
     }
+    return longKey.slice(0, message.length).toUpperCase();
+  }
+
+  encrypt(message, key) {
+    if (message && key) {
+      const longKey = this.createLongKey(message, key);
+      let encryptArr = [];
+      let indexKey = 0;
+      message
+        .toUpperCase()
+        .split("")
+        .forEach(item => {
+          if (this.alphabet.indexOf(item) !== -1) {
+            const encryptSymbol = this.alphabet[
+              (this.alphabet.indexOf(item) +
+                this.alphabet.indexOf(longKey[indexKey])) %
+                this.alphabet.length
+            ];
+            indexKey++;
+            encryptArr.push(encryptSymbol);
+          } else {
+            encryptArr.push(item);
+          }
+        });
+      return this.direct ? encryptArr.join("") : encryptArr.reverse().join("");
+    }
+    throw new Error();
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage && key) {
+      const longKey = this.createLongKey(encryptedMessage, key);
+      let encryptArr = [];
+      let indexKey = 0;
+      encryptedMessage
+        .toUpperCase()
+        .split("")
+        .forEach(item => {
+          if (this.alphabet.indexOf(item) !== -1) {
+            const encryptSymbol = this.alphabet[
+              (this.alphabet.indexOf(item) +
+                this.alphabet.length -
+                this.alphabet.indexOf(longKey[indexKey])) %
+                this.alphabet.length
+            ];
+            indexKey++;
+            encryptArr.push(encryptSymbol);
+          } else {
+            encryptArr.push(item);
+          }
+        });
+      return this.direct ? encryptArr.join("") : encryptArr.reverse().join("");
+    }
+    throw new Error();
+  }
 }
 
 module.exports = VigenereCipheringMachine;
